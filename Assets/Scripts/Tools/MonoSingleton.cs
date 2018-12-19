a0048fe4ebcebd8641de521deaeeb8c2651ef213
yangdongcheng <923092666@qq.com>
2018-12-19 17:15:19 +0800

    删除多余的脚本

---
+++ Assets/Scripts/Tools/MonoSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour
+    where T: MonoBehaviour
+    {
+    private static T mInstance;
+
+    public static T Instance
+    {
+        get
+        {
+            return mInstance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        mInstance = this as T;
+    }
+}
