494fe67f9a08ccb406a1ea1bd83eb7cdbfd5be0d
yangdongcheng <923092666@qq.com>
2018-12-19 22:37:18 +0800

    2018/12/19
    1.导入Dotween插件
    2.导入相关素材

---
+++ Assets/Scripts/Scenes/00_Home/HomeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MetalMax
+{
+    public class HomeManager : MainManager
+    {
+        protected override void LaunchInDevelopingMode()
+        {
+            //1.显示健康游戏提醒。使用Dotween。
+
+            //2.显示BG，播放音效，同时检查版本更新（右下角显示“正在检查更新……”）。
+        }
+
+        protected override void LaunchInProductionMode()
+        {
+            //1.显示健康游戏提醒。使用Dotween。
+
+            //2.显示BG，播放音效，同时检查版本更新（右下角显示“正在检查更新……”）。
+            throw new System.NotImplementedException();
+        }
+
+        protected override void LaunchInTestMode()
+        {
+            //1.显示健康游戏提醒。使用Dotween。
+
+            //2.显示BG，播放音效，同时检查版本更新（右下角显示“正在检查更新……”）。
+            throw new System.NotImplementedException();
+        }
+    }
+}
