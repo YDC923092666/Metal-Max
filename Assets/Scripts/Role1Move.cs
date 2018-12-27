a7878b9fd06469d0a87f4a69cb47269206e96050
yangdongcheng <923092666@qq.com>
2018-12-27 14:15:09 +0800

    2018/12/27

--- Assets/Scripts/Role1Move.cs
+++ Assets/Scripts/Role1Move.cs
@@ -17,18 +17,22 @@ namespace MetalMax
             if (Input.GetKeyDown(KeyCode.W))
             {
                 anim.SetTrigger("Up");
+                transform.Translate(Vector3.up);
             }
             if (Input.GetKeyDown(KeyCode.S))
             {
                 anim.SetTrigger("Down");
+                transform.Translate(Vector3.down);
             }
             if (Input.GetKeyDown(KeyCode.A))
             {
                 anim.SetTrigger("Left");
+                transform.Translate(Vector3.left);
             }
             if (Input.GetKeyDown(KeyCode.D))
             {
                 anim.SetTrigger("Right");
+                transform.Translate(Vector3.right);
             }
         }
     }
